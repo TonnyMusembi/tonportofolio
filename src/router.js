6c0b8047cf9688276da994d565ac4dc90a0c8b1b
TonnyMusembi <musembitonnyj@gamil.com>
2021-09-03 10:58:18 +0300

    commit

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+// 
+
+let router = new VueRouter({
+    // mode: 'history',
+    routes: [{
+            path: '/',
+            name: 'Landing',
+            component: () =>
+                import ('./components/Lander')
+        },
+        {
+            path: '/',
+            name: 'Login',
+            component: () =>
+                import ('./components/login')
+        },
+        // {
+        //     path: '/',
+        //     name: 'perfomance',
+        //     pages: () =>
+        //         import ('./pages/perfomance'
+        //         },
+    ]
+});
+export default router;
